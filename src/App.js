f56ea683c4cd3036402595c1f8ab325eef8a9654
Alexis Robles <alexis.robles49@gmail.com>
2022-10-19 13:09:29 -0500

    Added setInterval HTTP get request to prevent website from idling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import Title from './components/Title';
 
 function App() {
+
+  var http = require("http");
+  setInterval(function() {
+      http.get("http://www.alexis-robles.com/");
+  }, 300000);
+
   return (
     <>
       <Title/>
